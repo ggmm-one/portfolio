6867844242d957caea85c964b36784c5105b24bd
ggmm-one <ggmm@ggmm.one>
2019-09-23 11:26:35 +1000

    Remove unused traits

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class AuthController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use ValidatesRequests;
 }
