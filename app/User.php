0cc0a5fda571cb5c3d44949ce936a814cbe18b40
ggmm-one <ggmm@ggmm.one>
2019-09-24 19:22:39 +1000

    Remove dependency from Illuminate User

--- app/User.php
+++ app/User.php
@@ -2,19 +2,17 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as LaravelUser;
-use App\Traits\PublicAddressable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Traits\SessionFlashes;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends LaravelUser
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
-    use Notifiable;
-    use PublicAddressable;
-    use SoftDeletes;
-    use SessionFlashes;
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail; //from laravel default auth user
 
     public const DD_NAME_LENGTH = 256;
     public const DD_EMAIL_LENGTH = 256;
