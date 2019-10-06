de51db75eadedd6323471567ca10140698ce8a94
ggmm-one <ggmm@ggmm.one>
2019-10-06 19:14:30 +1100

    Fix collapse menu

--- resources/views/admin/inc/section_nav_bar.blade.php
+++ resources/views/admin/inc/section_nav_bar.blade.php
@@ -1,10 +1,10 @@
 @section('sectionnavbar')
-    <nav class="navbar navbar-expand-lg navbar-light bg-light app-nav-sub">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light app-nav-sub">
         <span class="navbar-brand">{{ __('Admin') }}</span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#adminnavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse" id="adminnavbar">
             <a class="nav-item nav-link" href="{{ route('admin.settings.edit') }}">{{ __('Settings') }}</a>
             <a class="nav-item nav-link" href="{{ route('admin.evaluation_items.index') }}">{{ __('Evaluation') }}</a>
             <a class="nav-item nav-link" href="{{ route('admin.resource_types.index') }}">{{ __('Resource Types') }}</a>
--- resources/views/layouts/frame_app.blade.php
+++ resources/views/layouts/frame_app.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.frame')
 
 @section('navbar')
-<div class="collapse navbar-collapse justify-content-between" id="headernavbar">
-    <div class="navbar-nav">
-        @can('portfoliosModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('portfolios.portfolios.index') }}">{{ __('Portfolios') }}</a> @endcan
-        @can('projectsModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('projects.projects.index') }}">{{ __('Projects') }}</a> @endcan
-        @can('resourcesModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('resources.root') }}">{{ __('Resources') }}</a> @endcan
-        @can('adminModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('admin.root') }}">{{ __('Admin') }}</a> @endcan
+    <div class="collapse navbar-collapse justify-content-between" id="headernavbar">
+        <div class="navbar-nav">
+            @can('portfoliosModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('portfolios.portfolios.index') }}">{{ __('Portfolios') }}</a> @endcan
+            @can('projectsModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('projects.projects.index') }}">{{ __('Projects') }}</a> @endcan
+            @can('resourcesModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('resources.root') }}">{{ __('Resources') }}</a> @endcan
+            @can('adminModule', App\User::class) <a class="nav-item nav-link app-nav-master-link" href="{{ route('admin.root') }}">{{ __('Admin') }}</a> @endcan
+        </div>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link app-nav-master-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path style="fill:#fff;" d="M16 9v-4l8 7-8 7v-4h-8v-6h8zm-16-7v20h14v-2h-12v-16h12v-2h-14z"/></svg>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+        </div>
     </div>
-    <div class="navbar-nav">
-        <a class="nav-item nav-link app-nav-master-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path style="fill:#fff;" d="M16 9v-4l8 7-8 7v-4h-8v-6h8zm-16-7v20h14v-2h-12v-16h12v-2h-14z"/></svg>
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
-    </div>
-</div>
 @endsection
 
 @section('frame')
