dace5e4123f6947a5a7669bb317440e599cab568
ggmm-one <ggmm@ggmm.one>
2019-10-19 10:12:41 +1100

    Update app config

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>@yield('pagetitle', 'Portfolio Management')</title>
+    <title>@yield('pagetitle', config('app.name'))</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/css/bootstrap.css" >
     <link rel="stylesheet" href="/css/css.css" >
 </head>
 <body id="@yield('bodyid')">
     <nav id="app-navbar" class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Portfolio Management</a>
+        <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
         <a href="#app-navbar" class="navbar-toggler app-navbar-toggler-open" type="button"
             aria-controls="app-navbar-nav" aria-expanded="false" aria-label="Open navigation">
             <span class="navbar-toggler-icon"></span>
--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.frame_app')
 
-@section('pagetitle', 'Portfolio Management')
 @section('bodyid', 'app-root')
 
 @section('content')
