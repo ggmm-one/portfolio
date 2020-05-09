fec918f7ffd26a2e77b8cf44886e888cd6c089ce
ggmm-one <ggmm@ggmm.one>
2020-05-09 15:50:10 +1000

    Set admin password

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,14 +10,15 @@ class UsersTableSeeder extends Seeder
     {
         $roles = Role::select('id')->where('name', '<>', 'Admin')->pluck('id');
         $admin = Role::select('id')->where('name', 'Admin')->pluck('id');
-        for ($i=0; $i < rand(10, 20); $i++) {
+        for ($i = 0; $i < rand(10, 20); $i++) {
             factory(User::class)->create([
-                'role_id' => $roles->random()
+                'role_id' => $roles->random(),
             ]);
         }
         factory(User::class)->create([
             'email' => 'admin@example.org',
-            'role_id' => $admin->random()
+            'password' => bcrypt('123123'),
+            'role_id' => $admin->random(),
         ]);
     }
 }
