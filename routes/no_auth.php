c3c21cb947e9418e80649867d5810127f4cf63cb
ggmm-one <ggmm@ggmm.one>
2020-12-26 21:09:19 +1100

    Upgrade files

--- routes/no_auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('auth/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('auth/login', 'Auth\LoginController@login');
-Route::get('auth/password/request', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('auth/password/request', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('auth/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('auth/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-Route::post('auth/logout', 'Auth\LoginController@logout')->name('logout');
 Route::view('/', 'root');
+
+Route::get('auth/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/password/request', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('auth/password/request', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('auth/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('auth/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::post('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
 //PORTFOLIOS SECTION
 
